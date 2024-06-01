71aedba345f8b351aeae31960f2baa2c27a7d31f
georgimnho <georgejp114@gmail.com>
2024-06-01 15:44:50 -0300

    Perfil
    template + static perfil

--- appyoutube/urls.py
+++ appyoutube/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import BaseView
+from .views import BaseView, PerfilView
 
 urlpatterns = [
  path('base/', BaseView.as_view(), name='home'),
+ path('perfil/', PerfilView.as_view(), name='perfil'),
+
 ]
--- appyoutube/views.py
+++ appyoutube/views.py
@@ -5,3 +5,9 @@ from django.views.generic import DetailView, ListView
 class BaseView(ListView):
     model = Video
     template_name = 'base.html'
+
+class PerfilView(ListView):
+    model = Video
+    template_name = 'modelo/perfil.html'
+
+
