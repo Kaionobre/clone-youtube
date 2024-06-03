b738bd2d8d9aad2cdc48cdb3b413873425df6fa5
Kaionobre <kaionobrega00@gmail.com>
2024-06-03 17:13:08 -0300

    ATT: atualizando o processo de login e cadastro do site

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from accounts.forms import LoginForms, CadastroForms
 from django.contrib.auth.models import User
 from django.contrib import auth
-
+from django.contrib import messages
 
 def login(request):
     form = LoginForms()
@@ -21,8 +21,10 @@ def login(request):
 
         if usuario is not None:
             auth.login(request, usuario)
+            messages.success(request, f"{nome} logado com sucesso!")
             return redirect('home')
         else:
+            messages.error(request, "Erro ao efeturar login")
             return redirect('login')
 
 
@@ -39,6 +41,7 @@ def cadastro(request):
         if form.is_valid():
 
             if form["senha_1"].value() != form["senha_2"].value():
+                messages.error(request, "Senhas não são iguais")
                 return redirect('cadastro')
             
             nome = form["nome_cadastro"].value()
@@ -46,6 +49,7 @@ def cadastro(request):
             senha = form["senha_1"].value()
 
             if User.objects.filter(username=nome).exists():
+                messages.error(request, "Usuário já existente")
                 return redirect('cadastro')
             
             usuario = User.objects.create_user(
@@ -56,9 +60,8 @@ def cadastro(request):
 
             usuario.save()
 
+            messages.success(request, "Cadastro efetuado com sucesso")
             return redirect('login')
         
 
     return render(request, 'accounts/pages/cadastro.html', {"form": form})
-
-
